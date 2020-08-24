8eeb7b345d210279dd3d1a788ae3a20e43797cda
kaede0902 <kaede0920js@gmail.com>
2020-08-24 17:01:24 +0900

    Updates

---
+++ precache-manifest.aa702f33a3e37c8bffc78813c0dddd03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f24fe91675d0c30a9dedebaa434236",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "0346286c8c6de227856b",
+    "url": "/portofolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portofolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portofolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0346286c8c6de227856b",
+    "url": "/portofolio/static/js/main.3b50dda9.chunk.js"
+  },
+  {
+    "revision": "14e8e72aa84fbcaa16d8",
+    "url": "/portofolio/static/js/runtime-main.ae760d9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portofolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
